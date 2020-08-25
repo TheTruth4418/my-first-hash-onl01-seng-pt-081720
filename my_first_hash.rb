3185660ab01eab8a84fdd2f9ac1b4899b4c411b4
William West <wwest466@gmail.com>
2020-08-25 22:43:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -25,8 +25,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
+shipping_manifest["oil paintings"]
 end
 
 def adding
@@ -36,7 +35,8 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets"] = 2
+shiping_manifest["gun powder"] = 4
 
 
   # add 4 gun powder to the shipping_manifest hash below
