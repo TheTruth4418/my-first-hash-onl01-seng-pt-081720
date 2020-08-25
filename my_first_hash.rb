8b85e9be6275c9ab14f098e483df2f189dbf064b
William West <wwest466@gmail.com>
2020-08-25 22:45:46 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,7 +36,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest["muskets"] = 2
-shipping_manifest["gun powder"] = 4
+
 
 
   # add 4 gun powder to the shipping_manifest hash below
@@ -44,5 +44,5 @@ shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+return shipping_manifest
 end
